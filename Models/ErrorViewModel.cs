938bdd9f2a1c8756a2cdf625cbc056982d7ab408
NQuynhAnh <nguyenquynh010920@gmail.com>
2023-01-04 08:05:49 +0700

    First_commit_NQA056

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenQuynhAnh056.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
